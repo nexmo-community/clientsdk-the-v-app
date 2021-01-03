bd9389faf08c6fc1459014c1af4c805d94c0294d
Paul Ardeleanu <paul.ardeleanu@vonage.com>
2021-01-03 23:01:46 +0000

    rtc webhook create_conversation

--- server-rails/app/controllers/webhook_controller.rb
+++ server-rails/app/controllers/webhook_controller.rb
@@ -2,19 +2,56 @@ class WebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   def rtc_events
-    
     # "type": "conversation:created"
     # "application_id": "19e0465b-7cd9-4e7e-96d8-c7942a9bdb87",
     # puts request.raw_post
-    json = JSON.parse(request.body.read)
-    byebug
-    if ENV['VONAGE_APP_ID'] != json['application_id']
-      render plain: 'Wrong application', status: 418
+    begin
+      json = JSON.parse(request.body.read)
+    rescue JSON::ParserError
+      render plain: 'Invalid JSON'
       return
+    end
+    
+    if ENV['VONAGE_APP_ID'] != json['application_id']
+      render plain: 'Wrong application'
+      return
+    end
+    event_type = json["type"]
+    event_body = json["body"]
+    # check if not nil
+    case event_type
+    when "conversation:created"
+      conversation = create_conversation(event_body)
+      if conversation.blank?
+        # record error
+        render plain: 'Invalid conversation details'
+        return
+      end
     end
     render plain: 'ok'
     
   end
 
   
+  private 
+  def create_conversation(conversation_json)
+    # "id": "CON-b273a8fc-c4f3-45bf-a059-139e925ce5d7",
+    # "name": "NAM-ec666b5d-10f4-4b02-94a3-faf50e46e591",
+    # "timestamp": {
+    #     "created": "2020-12-14T14:35:24.457Z"
+    # },
+    # "display_name": "twitch 20201214-001",
+    # "state": "ACTIVE"
+    if conversation_json.empty? ||conversation_json["id"].empty? || conversation_json["name"].empty? || conversation_json["state"].empty? 
+      return nil
+    end
+    conversation = Conversation.create(
+      vonage_id: conversation_json["id"], 
+      name: conversation_json["name"], 
+      display_name: conversation_json["display_name"], 
+      state: conversation_json["state"],
+      vonage_created_at: conversation_json["timestamp"]["created"])
+
+    return conversation
+  end
 end
--- server-rails/spec/requests/webhook_request_spec.rb
+++ server-rails/spec/requests/webhook_request_spec.rb
@@ -6,32 +6,55 @@ RSpec.describe "Webhooks", type: :request do
     
     it " - invalid application id" do
       post rtc_events_path, params: '{"application_id": "19e0465b-7cd9-4e7e-96d8-c7942a9bdb88"}', as: :json
-      expect(response).to have_http_status(418)
+      expect(response).to have_http_status(200)
     end
     
     context 'new conversation' do
 
       context 'success' do
         it " - returns success" do
-          post rtc_events_path, params: VCR.load('webhooks/rtc_events/new_conversation'), as: :json
+          post rtc_events_path, params: VCR.load('webhooks/rtc_events/new_conversation'), headers: { 'Content-Type' => 'application/json' }
           expect(response).to have_http_status(200)
         end
+        it " - creates a conversation in the DB" do
+          expect {
+            post rtc_events_path, params: VCR.load('webhooks/rtc_events/new_conversation'), headers: { 'Content-Type' => 'application/json' }
+          }.to change { Conversation.count }.by 1
+          new_conversation = Conversation.last
+          expect(new_conversation.vonage_id).to eq("CON-b273a8fc-c4f3-45bf-a059-139e925ce5d7")
+          expect(new_conversation.name).to eq("NAM-ec666b5d-10f4-4b02-94a3-faf50e46e591")
+          expect(new_conversation.display_name).to eq("twitch 20201214-001")
+          expect(new_conversation.state).to eq("ACTIVE")
+          expect(new_conversation.vonage_created_at).to eq("2020-12-14T14:35:24.457Z")
+        end
+
       end
 
       context 'error' do
         
-
-
         it " - invalid event type" do
-
+          expect {
+            post_json = VCR.load('webhooks/rtc_events/new_conversation')
+            post rtc_events_path, params: post_json.sub('"type": "conversation:created"', '"type": "conversation:create"'), headers: { 'Content-Type' => 'application/json' }
+          }.to change { Conversation.count }.by 0
         end
 
         it " - invalid body - empty" do
-          
+          expect {
+            post rtc_events_path, params: '{
+              "body": {
+              },
+              "application_id": "19e0465b-7cd9-4e7e-96d8-c7942a9bdb87",
+              "timestamp": "2020-12-14T14:35:24.463Z",
+              "type": "conversation:created"
+            }', headers: { 'Content-Type' => 'application/json' }
+          }.to change { Conversation.count }.by 0
         end
 
         it " - invalid body - invalid JSON" do
-          
+          expect {
+            post rtc_events_path, params: 'asdfasdfasdasd'
+          }.to_not change { Conversation.all.to_json }
         end
       end
 
