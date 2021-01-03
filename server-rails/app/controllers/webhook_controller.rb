1ef14d242ca4a2058f4444ea7ce6f2c655406ff5
Paul Ardeleanu <paul.ardeleanu@vonage.com>
2021-01-03 22:36:47 +0000

    rtc_events webhook new conversation

--- server-rails/app/controllers/webhook_controller.rb
+++ server-rails/app/controllers/webhook_controller.rb
@@ -2,8 +2,19 @@ class WebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   def rtc_events
-    puts request.raw_post
+    
+    # "type": "conversation:created"
+    # "application_id": "19e0465b-7cd9-4e7e-96d8-c7942a9bdb87",
+    # puts request.raw_post
+    json = JSON.parse(request.body.read)
+    byebug
+    if ENV['VONAGE_APP_ID'] != json['application_id']
+      render plain: 'Wrong application', status: 418
+      return
+    end
     render plain: 'ok'
+    
   end
 
+  
 end
--- server-rails/spec/requests/webhook_request_spec.rb
+++ server-rails/spec/requests/webhook_request_spec.rb
@@ -2,11 +2,46 @@ require 'rails_helper'
 
 RSpec.describe "Webhooks", type: :request do
 
-  describe "GET /rtc_events" do
-    it "returns http success" do
-      # get "/webhook/rtc_events"
-      # expect(response).to have_http_status(:success)
+  describe "GET /rtc/events" do
+    
+    it " - invalid application id" do
+      post rtc_events_path, params: '{"application_id": "19e0465b-7cd9-4e7e-96d8-c7942a9bdb88"}', as: :json
+      expect(response).to have_http_status(418)
     end
+    
+    context 'new conversation' do
+
+      context 'success' do
+        it " - returns success" do
+          post rtc_events_path, params: VCR.load('webhooks/rtc_events/new_conversation'), as: :json
+          expect(response).to have_http_status(200)
+        end
+      end
+
+      context 'error' do
+        
+
+
+        it " - invalid event type" do
+
+        end
+
+        it " - invalid body - empty" do
+          
+        end
+
+        it " - invalid body - invalid JSON" do
+          
+        end
+      end
+
+    end
+
+
+    context 'new member join' do
+
+    end
+
   end
 
 end
