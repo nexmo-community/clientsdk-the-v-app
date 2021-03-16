81153326f3850fe00c0c0a9304d027d2e0377120
igor.wojda <igor.wojda@gmail.com>
2021-03-16 17:26:16 +0100

    Update layouts

--- client-android/app/src/main/java/com/vonage/vapp/presentation/MainFragment.kt
+++ client-android/app/src/main/java/com/vonage/vapp/presentation/MainFragment.kt
@@ -1,9 +1,7 @@
 package com.vonage.vapp.presentation
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.vonage.vapp.R
@@ -13,14 +11,6 @@ import com.vonage.vapp.utils.viewBinding
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     private val binding: FragmentMainBinding by viewBinding()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
