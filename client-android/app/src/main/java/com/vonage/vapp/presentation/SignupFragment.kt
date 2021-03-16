80e01ebf23f83f03fac5284c00dd5e875afe3aa8
igor.wojda <igor.wojda@gmail.com>
2021-03-16 16:45:41 +0100

    Update SignupFragment fragment

--- client-android/app/src/main/java/com/vonage/vapp/presentation/SignupFragment.kt
+++ client-android/app/src/main/java/com/vonage/vapp/presentation/SignupFragment.kt
@@ -2,10 +2,14 @@ package com.vonage.vapp.presentation
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.vonage.vapp.R
+import com.vonage.vapp.data.BackendRepository
 import com.vonage.vapp.databinding.FragmentSignupBinding
 import com.vonage.vapp.utils.viewBinding
+import kotlinx.coroutines.launch
 
 class SignupFragment : Fragment(R.layout.fragment_signup) {
 
@@ -13,5 +17,35 @@ class SignupFragment : Fragment(R.layout.fragment_signup) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.signUpButton.setOnClickListener {
+            signUp()
+        }
+    }
+
+    private fun signUp() {
+        if (isTextViewEmpty(binding.nameTextView) ||
+            isTextViewEmpty(binding.displayNameTextView) ||
+            isTextViewEmpty(binding.passwordTextView)
+        ) {
+
+            binding.messageTextView.text = "Please fill all fields"
+
+            return
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val respnse = BackendRepository.signup(
+                binding.nameTextView.text.toString(),
+                binding.displayNameTextView.text.toString(),
+                binding.passwordTextView.text.toString()
+            )
+
+            val a  = 2
+        }
+    }
+
+    private fun isTextViewEmpty(textView: TextView): Boolean {
+        return textView.text.toString().isEmpty()
     }
 }
