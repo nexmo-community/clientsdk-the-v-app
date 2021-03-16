a9bf66f168755df19335e1b06cb9c144ef4b6d1c
igor.wojda <igor.wojda@gmail.com>
2021-03-16 16:27:46 +0100

    Update layout

--- client-android/app/src/main/java/com/vonage/vapp/presentation/MainActivity.kt
+++ client-android/app/src/main/java/com/vonage/vapp/presentation/MainActivity.kt
@@ -1,16 +1,6 @@
 package com.vonage.vapp.presentation
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.nexmo.client.NexmoClient
 import com.vonage.vapp.R
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var client: NexmoClient
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
