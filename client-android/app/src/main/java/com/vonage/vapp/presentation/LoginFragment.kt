f6527af2bd20eac5a734cdf25375c4024463619c
igor.wojda <igor.wojda@gmail.com>
2021-03-17 08:35:14 +0100

    Add login form validation

--- client-android/app/src/main/java/com/vonage/vapp/presentation/LoginFragment.kt
+++ client-android/app/src/main/java/com/vonage/vapp/presentation/LoginFragment.kt
@@ -1,10 +1,42 @@
 package com.vonage.vapp.presentation
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.vonage.vapp.R
 import com.vonage.vapp.databinding.FragmentLoginBinding
 import com.vonage.vapp.utils.viewBinding
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
     private val binding: FragmentLoginBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginButton.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        updateTextView(binding.nameTextView, "Enter name")
+        updateTextView(binding.passwordTextView, "Enter password")
+
+        if(binding.nameTextView.error != null || binding.passwordTextView.error != null) {
+            return
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            // do login
+        }
+    }
+
+    private fun updateTextView(textView: TextView, errorMessage: String) {
+        textView.apply {
+            error = if(text.toString().isEmpty()) errorMessage else null
+        }
+    }
 }
