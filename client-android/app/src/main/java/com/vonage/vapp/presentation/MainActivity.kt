4d80df0119d28eaaa4167eb68606bae3651cb6ff
igor.wojda <igor.wojda@gmail.com>
2021-03-18 11:42:26 +0100

    Open conversation

--- client-android/app/src/main/java/com/vonage/vapp/presentation/MainActivity.kt
+++ client-android/app/src/main/java/com/vonage/vapp/presentation/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.vonage.vapp.presentation
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.nexmo.client.NexmoClient
 import com.vonage.vapp.R
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Init the client
+        // It can le latte raccessed by calling NexmoClient.get()
+        NexmoClient.Builder().build(this)
+    }
+}
