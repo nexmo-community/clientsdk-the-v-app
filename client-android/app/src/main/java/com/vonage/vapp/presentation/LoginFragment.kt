d05a46747aa8b3321bdf9da1cf3fb0df8da8d42f
igor.wojda <igor.wojda@gmail.com>
2021-03-18 14:14:02 +0100

    Auto login

--- client-android/app/src/main/java/com/vonage/vapp/presentation/LoginFragment.kt
+++ client-android/app/src/main/java/com/vonage/vapp/presentation/LoginFragment.kt
@@ -30,6 +30,8 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         binding.signupButton.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_SignupFragment)
         }
+
+        login()
     }
 
     private fun login() {
