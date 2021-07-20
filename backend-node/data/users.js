04618c1d91459c2b3acaf41da5b03c1c02dd7521
Paul Ardeleanu <pardel@gmail.com>
2021-07-20 13:34:01 +0100

    sync users

--- backend-node/data/users.js
+++ backend-node/data/users.js
@@ -1,19 +1,15 @@
 const crypto = require('crypto');
-const { Pool } = require('pg');
+const { Pool, Client } = require('pg');
 
-const connectionString = process.env.postgresDatabaseUrl;
-const pool = new Pool({
-  connectionString
-});
 const passwordSalt = process.env.salt;
 
 const Vonage = require('../vonage');
 
 
 
-const getAll = async function () {
+const getAll = async function (client) {
   try {
-    const res = await pool.query('SELECT vonage_id, name, display_name FROM users');
+    const res = await client.query('SELECT vonage_id, name, display_name FROM users');
     if (res.rowCount > 0 ) {
       return res.rows;
     }
@@ -24,15 +20,15 @@ const getAll = async function () {
 }
 
 
-const getInterlocutorsFor = async function (user_name) {
-  const users = await getAll();
+const getInterlocutorsFor = async function (client, user_name) {
+  const users = await getAll(client);
   return users.filter(f => f.name !== user_name).map(u => { 
     return { 'id': u.vonage_id, 'name': u.name, 'display_name': u.display_name } 
   })
 }
 
 
-const create = async function (vonage_id, name, display_name, password) {
+const create = async function (client, vonage_id, name, display_name, password) {
   let user;
   name = name.toLowerCase();
   user = await getByName(name);
@@ -47,7 +43,7 @@ const create = async function (vonage_id, name, display_name, password) {
         .update(password)
         .digest('hex');
     }
-    const res = await pool.query('INSERT INTO users(vonage_id, name, display_name, password_digest) VALUES($1, $2, $3, $4) RETURNING vonage_id, name, display_name', [vonage_id, name, display_name, passwordHash]);
+    const res = await client.query('INSERT INTO users(vonage_id, name, display_name, password_digest) VALUES($1, $2, $3, $4) RETURNING vonage_id, name, display_name', [vonage_id, name, display_name, passwordHash]);
     if (res.rowCount === 1) {
       user = res.rows[0];
       user.status = 'created';
@@ -60,7 +56,7 @@ const create = async function (vonage_id, name, display_name, password) {
 }
 
 
-const addPassword = async function (name, password) {
+const addPassword = async function (client, name, password) {
   let user;
   name = name.toLowerCase();
   user = await getByName(name);
@@ -71,19 +67,19 @@ const addPassword = async function (name, password) {
         .update(password)
         .digest('hex');
     }
-    const res = await pool.query('UPDATE users SET password_digest=$1 WHERE name=$2', [passwordHash, name]);
+    const res = await client.query('UPDATE users SET password_digest=$1 WHERE name=$2', [passwordHash, name]);
   }
 }
 
 
-const authenticate = async function (name, password) {
+const authenticate = async function (client, name, password) {
   let user;
   try {
     name = name.toLowerCase();
     const passwordHash = crypto.createHash('sha256', passwordSalt)
       .update(password)
       .digest('hex');
-    const res = await pool.query('SELECT vonage_id, name, display_name from users where name=$1::text and password_digest=$2::text', [name, passwordHash]);
+    const res = await client.query('SELECT vonage_id, name, display_name from users where name=$1::text and password_digest=$2::text', [name, passwordHash]);
 
     if (res.rowCount === 1) {
       user = res.rows[0];
@@ -92,81 +88,81 @@ const authenticate = async function (name, password) {
   } catch (err) {
     console.log(err);
   }
-
   return user;
 }
 
 
-const getByName = async function (name, apiFallback = false) {
+const getByName = async function (client, name) {
   let user;
   try {
     name = name.toLowerCase();
-    const res = await pool.query('SELECT vonage_id, name, display_name, password_digest from users where name=$1', [name]);
+    const res = await client.query('SELECT vonage_id, name, display_name, password_digest from users where name=$1', [name]);
     if (res.rowCount === 1) {
       user = res.rows[0];
     }
   } catch (err) {
     console.log(err);
   }
-  if(user || !apiFallback) {
-    return user;
-  }
-  await sync();
-  return await getByName(name, false);
+  return user;
 }
 
 
-const getByVonageId = async function (vonageId, apiFallback = false) {
+const getByVonageId = async function (client, vonageId) {
   let user;
   try {
-    const res = await pool.query('SELECT vonage_id, name, display_name, password_digest from users where vonage_id=$1', [vonageId]);
+    const res = await client.query('SELECT vonage_id, name, display_name, password_digest from users where vonage_id=$1', [vonageId]);
     if (res.rowCount === 1) {
       user = res.rows[0];
     }
   } catch (err) {
     console.log(err);
   }
-  if(user || !apiFallback) {
-    return user;
-  }
-  await sync();
-  return await getByVonageId(vonageId, false);
+  return user;
 }
 
 
-const sync = async function () {
+const syncAll = async function () {
+  const pool = new Pool({ connectionString: process.env.postgresDatabaseUrl });
+  pool.connect(async (err, client, done) => {
+    if (err) throw err
+    let vonageUsers = await Vonage.users.getAll();
+    console.log("retrieved " + vonageUsers.length + " vonage users");
 
-  let vonageUsers = await Vonage.users.getAll();
-
-  // console.log(vonageUsers);
-  for(let i = 0; i < vonageUsers.length; i++) {
-    let vonageUser = vonageUsers[i];
-    if(!vonageUser || !vonageUser.vonage_id || !vonageUser.name || !vonageUser.display_name) {
-      return;
-    }
-    // find user in DB (use name as key)
-    vonageUserName = vonageUser.name.toLowerCase()
-    let user = await getByName(vonageUserName)
-    // if not present (insert)
-    if(!user) {
-      user = await create(vonageUser.vonage_id, vonageUser.name, vonageUser.display_name, null);
-    } else {
-      // if present (update)
-      // console.log(`USER PRESENT - ${user.vonage_id}\t${user.name} \t ${user.display_name}`);
-      try {
-        // console.log(`  UPDATE WITH - ${vonageUser.vonage_id}\t${vonageUser.name} \t ${vonageUser.display_name}`);
-        const res = await pool.query('UPDATE users SET vonage_id=$1, display_name=$2 WHERE name=$3 RETURNING vonage_id, name, display_name', [vonageUser.vonage_id, vonageUser.display_name, vonageUserName]);
-        // console.log(res);
-        if (res.rowCount === 1) {
-          user = res.rows[0];
-          console.log(`USER UPDATED - ${user.vonage_id}\t${user.name} \t ${user.display_name}`);
-        }
-      } catch (err) {
-        console.log(err);
+    for(let i = 0; i < vonageUsers.length; i++) {
+      let vonageUser = vonageUsers[i];
+      if(!vonageUser || !vonageUser.vonage_id || !vonageUser.name || !vonageUser.display_name) {
+        return;
       }
-    }
-  };
+      await syncUser(client, vonageUser);
+    };
+    console.log("All done");
+    client.release();
+  });
+  pool.end()
 }
+
+const syncUser = async function (client, vonageUser) {
+  // find user in DB (use name as key)
+  vonageUserName = vonageUser.name.toLowerCase();
+  let user = await getByName(client, vonageUserName);
+  if(!user) {
+    // if not present (insert)
+    const res = await client.query('INSERT INTO users(vonage_id, name, display_name, created_at, updated_at) VALUES($1, $2, $3, NOW(), NOW()) RETURNING vonage_id, name, display_name', [vonageUser.vonage_id, vonageUser.name, vonageUser.display_name]);
+    if (res.rowCount === 1) {
+      user = res.rows[0];
+    }
+  } else {
+    // if present (update)
+    const res = await client.query('UPDATE users SET vonage_id=$1, display_name=$2, updated_at=NOW() WHERE name=$3 RETURNING vonage_id, name, display_name', [vonageUser.vonage_id, vonageUser.display_name, vonageUserName]);
+    // console.log(res);
+    if (res.rowCount === 1) {
+      user = res.rows[0];
+      console.log(`USER UPDATED - ${user.vonage_id}\t${user.name} \t ${user.display_name}`);
+    }
+  }
+  return user
+}
+
 
 module.exports = {
   getAll,
@@ -176,5 +172,5 @@ module.exports = {
   getByName,
   getByVonageId,
   authenticate,
-  sync
+  syncAll
 }
