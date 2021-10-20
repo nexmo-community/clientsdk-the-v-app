d7b4c569780c1ec0cf6e59b086a14d941b4d4c49
Dwane Hemmings <dwane.hemmings@vonage.com>
2021-10-20 11:48:54 -0400

    Update backend-node/index.js

--- backend-node/index.js
+++ backend-node/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
